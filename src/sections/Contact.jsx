b8d000c1bbaffe1b85fe945b40c6b028cd02f840
Sayan8945 <sayanpub2020@gmail.com>
2026-06-29 21:37:51 +0530

    Save current work

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -26,9 +26,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'Sayan Sarkar',
+          to_name: 'JavaScript Mastery',
           from_email: form.email,
-          to_email: 'sayanpub2020@gmail.com',
+          to_email: 'sujata@jsmastery.pro',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
